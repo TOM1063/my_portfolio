7f8e425de4a433ba830388f4ab7bcc9cbaeb1d15
Tom <71088450+TOM1063@users.noreply.github.com>
2021-05-31 16:56:26 +0900

    ritch.js

---
+++ ritch.js
@@ -0,0 +1,12 @@
+$(function(){
+    $(window).scroll(function (){
+        $('.left-to-right, .down-to-top').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 150){
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
